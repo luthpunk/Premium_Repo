d57bd7d572ba6af79a818da74c511fbe407d6372
amanhnb88 <aldry.manu02@gmail.com>
2026-04-08 00:57:39 +0900

    Update IdlixProviderPlugin.kt

--- IdlixProvider/src/main/kotlin/com/IdlixProvider/IdlixProviderPlugin.kt
+++ IdlixProvider/src/main/kotlin/com/IdlixProvider/IdlixProviderPlugin.kt
@@ -1,18 +1,12 @@
-package com.michat88
+package com.IdlixProvider
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-// Anotasi ini memberi tahu sistem Cloudstream bahwa kelas ini adalah sebuah plugin
 @CloudstreamPlugin
 class IdlixProviderPlugin: BasePlugin() {
-    
-    // Fungsi load() adalah titik awal yang akan dieksekusi saat plugin dimuat oleh aplikasi
     override fun load() {
-        // Mendaftarkan provider utama yang bertugas melakukan scraping data dari website (Idlix)
         registerMainAPI(IdlixProvider())
-        
-        // Mendaftarkan extractor yang bertugas mengambil link video langsung dari host (Jeniusplay)
         registerExtractorAPI(Jeniusplay())
     }
 }
