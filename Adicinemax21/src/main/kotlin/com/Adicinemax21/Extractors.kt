58024be69a225d0ec8c79385690261a69111d18f
amanhnb88 <aldry.manu02@gmail.com>
2026-04-11 02:10:11 +0900

    Update Extractors.kt

--- Adicinemax21/src/main/kotlin/com/Adicinemax21/Extractors.kt
+++ Adicinemax21/src/main/kotlin/com/Adicinemax21/Extractors.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.newSubtitleFile
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
 
@@ -19,10 +20,8 @@ class Jeniusplay : ExtractorApi() {
         callback: (ExtractorLink) -> Unit
     ) {
         try {
-            // 1. Ambil HTML dari Jeniusplay
             val htmlContent = app.get(url, referer = referer).text
             
-            // 2. Ekstrak Subtitle
             val subtitleRegex = """var\s+playerjsSubtitle\s*=\s*["'](.*?)["']""".toRegex()
             subtitleRegex.find(htmlContent)?.groupValues?.get(1)?.let { subStr ->
                 val tracks = subStr.split(",")
@@ -31,21 +30,16 @@ class Jeniusplay : ExtractorApi() {
                     if (langMatch != null) {
                         val lang = getLanguage(langMatch.groupValues[1])
                         val subUrl = langMatch.groupValues[2]
-                        subtitleCallback.invoke(SubtitleFile(lang, subUrl))
+                        subtitleCallback.invoke(newSubtitleFile(lang, subUrl))
                     }
                 }
             }
 
-            // 3. Bongkar Javascript Packer
             val unpackedText = getAndUnpack(htmlContent).replace("\\/", "/")
-            
-            // 4. Cari Link Video (Fokus ekstensi master.txt / .m3u8)
             val videoRegex = """(https?://[^"'\s]+(?:master\.txt|\.m3u8))""".toRegex()
             var videoUrl = videoRegex.find(unpackedText)?.groupValues?.get(1)
 
-            // 5. Fallback ke API do=getVideo
             if (videoUrl.isNullOrEmpty()) {
-                Log.d("adixtream", "Unpack JS gagal, mencoba fallback ke API do=getVideo...")
                 val hashRegex = """([a-zA-Z0-9]{30,})""".toRegex()
                 var hash = url.substringAfter("data=", "").substringBefore("&")
                 
@@ -63,19 +57,12 @@ class Jeniusplay : ExtractorApi() {
                 
                 val rawVideoSource = apiResponse?.videoSource
                 if (!rawVideoSource.isNullOrEmpty()) {
-                    // Bypass ekstensi woff/txt dari Cloudflare
                     videoUrl = rawVideoSource.replace(".woff", ".m3u8").replace(".txt", ".m3u8")
                 }
             }
 
-            // 6. Lempar Link ke ExtractorLink
             if (!videoUrl.isNullOrEmpty()) {
-                Log.d("adixtream", "Jeniusplay berhasil menemukan video: $videoUrl")
-                
-                // Ekstraktor M3U8
                 generateM3u8(name, videoUrl, mainUrl).forEach(callback)
-                
-                // Ekstraktor Direct
                 callback.invoke(
                     newExtractorLink(
                         source = name,
@@ -87,13 +74,9 @@ class Jeniusplay : ExtractorApi() {
                         this.referer = url 
                     }
                 )
-            } else {
-                Log.d("adixtream", "Jeniusplay gagal mendapat videoSource sama sekali.")
             }
-
         } catch (e: Exception) {
-            Log.e("adixtream", "Jeniusplay Error: ${e.message}")
-            e.printStackTrace()
+            Log.e("Adicinemax21", "Jeniusplay Error: ${e.message}")
         }
     }
 
@@ -106,7 +89,6 @@ class Jeniusplay : ExtractorApi() {
     }
 }
 
-// Data Class Fallback Jeniusplay
 data class ResponseSource(
     @JsonProperty("videoSource") val videoSource: String = ""
 )
